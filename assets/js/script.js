6adb688db027b35edb395fe0e1966d29e01d1e58
Samet Eren Terzi <s4metet@gmail.com>
2025-02-20 22:39:41 +0300

    Hatalar Düzeltildi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
 function showSidebar() {
   const sidebar = document.querySelector('.sidebar');
   sidebar.style.display = 'flex';
+  setTimeout(() => {
+      sidebar.style.opacity = '1';
+      sidebar.style.transform = 'translateX(0)';
+  }, 10);
 }
 
 function hideSidebar() {
   const sidebar = document.querySelector('.sidebar');
-  sidebar.style.display = 'none';
+  sidebar.style.opacity = '0';
+  sidebar.style.transform = 'translateX(100%)';
+
+  setTimeout(() => {
+      sidebar.style.display = 'none';
+  }, 500);
 }
+
 
 document.addEventListener("DOMContentLoaded", function () {
   const links = document.querySelectorAll('nav a[href^="#"]');
