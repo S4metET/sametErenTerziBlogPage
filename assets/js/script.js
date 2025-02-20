2ec4e1672ae7ea62c0618ed684b04c644af60d68
Samet Eren Terzi <s4metet@gmail.com>
2025-02-20 22:12:02 +0300

    Hatalar Düzeltildi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,28 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex'
-  }
-  function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'
-  }
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll('nav a[href^="#"]');
+
+  links.forEach(link => {
+      link.addEventListener("click", function (event) {
+          event.preventDefault();
+          const targetId = this.getAttribute("href").substring(1);
+          const targetElement = document.getElementById(targetId);
+
+          if (targetElement) {
+              targetElement.scrollIntoView({
+                  behavior: "smooth",
+                  block: "start"
+              });
+          }
+      });
+  });
+});
